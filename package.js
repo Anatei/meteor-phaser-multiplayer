d967d92378475aa46e4a4f1bcdb5ab7194daa193
Anatei <anateikim@gmail.com>
2015-11-21 04:25:53 +0200

    Update package.js

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-  name: 'robertlowe:meteor-phaser',
+  name: 'anatei:meteor-phaser-multiplayer',
   summary: 'Phaser.io packaged for meteor',
   version: '2.2.1',
-  git: 'https://github.com/robertlowe/meteor-phaser.git'
+  git: 'https://github.com/Anatei/meteor-phaser-multiplayer.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('phaser/build/phaser.js', 'client', {bare: true});
+  api.addFiles('phaser/build/phaser.js', ['client', 'server'], {bare: true});
 });
 
